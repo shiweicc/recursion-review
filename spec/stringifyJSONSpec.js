d2f6d1dbe256876cfd59ec0a7bcc2713bd7f64e9
Shiwei Chen <shiweichenus@gmail.com>
2022-02-26 14:47:39 -0600

    Complete Bare Minimum Requirements

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,43 +1,20 @@
 // test cases are described in fixtures.js
-// describe('stringifyJSON', function() {
-//   it('should match the result of calling JSON.stringify', function() {
-
-//     stringifiableObjects.forEach(function(test) {
-//       var expected = JSON.stringify(test);
-//       var result = stringifyJSON(test);
-//       expect(result).to.equal(expected);
-//     });
-
-//     unstringifiableValues.forEach(function(obj) {
-//       var expected = JSON.stringify(obj);
-//       var result = stringifyJSON(obj);
-//       expect(result).to.equal(expected);
-//     });
-
-//   });
-// });
-
-
-// test cases are described in fixtures.js
 describe('stringifyJSON', function() {
-  describe('should match the result of calling JSON.stringify', function() {
+  it('should match the result of calling JSON.stringify', function() {
 
     stringifiableObjects.forEach(function(test) {
-      it(JSON.stringify(test), function() {
-        var expected = JSON.stringify(test);
-        var result = stringifyJSON(test);
-        expect(result).to.equal(expected);
-      });
+      var expected = JSON.stringify(test);
+      var result = stringifyJSON(test);
+      expect(result).to.equal(expected);
     });
-  });
 
-  describe('should not stringify invalid input (functions, undefined)', function() {
-    unstringifiableValues.forEach(function(item) {
-      it(JSON.stringify(item) + '', function() {
-        var expected = JSON.stringify(item);
-        var result = stringifyJSON(item);
-        expect(result).to.equal(expected);
-      });
+    unstringifiableValues.forEach(function(obj) {
+      var expected = JSON.stringify(obj);
+      var result = stringifyJSON(obj);
+      expect(result).to.equal(expected);
     });
+
   });
 });
+
+
